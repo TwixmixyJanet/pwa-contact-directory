0fbfd19810aa5f5bf95cef1fb31ce24cd4a648ce
Janet Webster <twixmixy@gmail.com>
2023-11-28 14:27:42 -0500

    Add webpack configuration for PWA Contact Directory

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,17 +11,66 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "PWA Contact Directory"
+      }),
+
+      // Custom service worker injected
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js"
+      }),
+
+      // manifest.json file created
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "PWA Contact Directory",
+        short_name: "pwa-contact-directory",
+        description: "PWA Contact Directory built with Webpack",
+        background_color: "#272822",
+        theme_color: "#31a9e1",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          }
+        ]
+      })
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/present-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
     }
   };
 };
